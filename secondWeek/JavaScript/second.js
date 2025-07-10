fa289081ab319feddb23c77373fbdf614de950d4
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-07-10 22:33:08 +0600

    second.js

---
+++ secondWeek/JavaScript/second.js
@@ -0,0 +1,11 @@
+const name = 'Rayhan';
+const age = 23;
+const commit = true;
+
+function Towhid(username, userage, usercommit){
+    return (
+        'Name is: ' + username + 'His age is: ' + userage + ', He is loyal: ' + usercommit
+    );
+}
+
+console.log(Towhid(name, age, commit));
