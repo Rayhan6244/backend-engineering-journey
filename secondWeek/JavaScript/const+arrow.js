2b11b2f9855fe572a112645d8d96d29f83ae286c
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-07-10 22:45:04 +0600

    const+arrow.js

--- secondWeek/JavaScript/const+arrow.js
+++ secondWeek/JavaScript/const+arrow.js
@@ -1,14 +1,14 @@
-const name = 'Rayhan and Isha';
+const name = 'Rayhan';
 const age = 45;
-const marriage = true;
+const hobby = true;
 
-const towhid = (bothname, bothage, bothmarried) => {
+const towhid = (bothname, bothage, huda) => {
     return (
-        'Their Name is: ' + bothname + ', Their both age: ' + bothage + ', Are they married?: ' + bothmarried
+        'Name is: ' + bothname + ', Their both age: ' + bothage + ', His Hobby Has a Meaning: ' + huda
     );
 }
 
-console.log(towhid(name, age, marriage));
+console.log(towhid(name, age, hobby));
 
 const add = (r, i) => ('The sum is: '+ (r + i));
 
