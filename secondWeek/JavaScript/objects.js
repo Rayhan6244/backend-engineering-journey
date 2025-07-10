13b83b41d6acddf6f5871aaddbf215c3813ad7f0
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-07-10 22:32:20 +0600

    objects.js

---
+++ secondWeek/JavaScript/objects.js
@@ -0,0 +1,10 @@
+const rayhan = {
+    name: 'Rayhan',
+    age: 23,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }    
+    //console.log('Hi I am ' + this.name );}
+};
+//console.log(rayhan);
+rayhan.greet();
