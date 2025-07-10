e232c9b8eb58b1d08b2ed59235aec997bca974e4
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-07-10 22:34:51 +0600

    third.js

---
+++ secondWeek/JavaScript/third.js
@@ -0,0 +1,15 @@
+const name = 'Rayhan and Isha';
+const age = 45;
+const marriage = true;
+
+const towhid = (bothname, bothage, bothmarried) => {
+    return (
+        'Their Name is: ' + bothname + ', Their both age: ' + bothage + ', Are they married?: ' + bothmarried
+    );
+}
+
+console.log(towhid(name, age, marriage));
+
+const add = (r, i) => ('The sum is: '+ (r + i));
+
+console.log(add(7,25));
